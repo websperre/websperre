c53465f58325452a8f703e08d49cbb7a93af7318
m <49080343+opp@users.noreply.github.com>
2025-02-05 20:42:15 -0500

    update lite version to stop bg script termination
    update linked urls
    
    .. it might be time

--- lite/src/background.js
+++ lite/src/background.js
@@ -8,22 +8,11 @@ const handleRequest = (details) => {
     return { redirectUrl: blockedRequestRedirect };
 };
 
-browser.runtime.onInstalled.addListener(() => {
-    updateBlockedUrlsListener();
-});
-
-const handleStorageChange = (changes, areaName) => {
-    if (areaName === "local" && "gs" in changes) {
-        updateBlockedUrlsListener();
-    }
-};
-browser.storage.onChanged.addListener(handleStorageChange);
-
 let dGesperrtSeiten = [];
 const updateBlockedUrlsListener = async () => {
     try {
         const getGs = await browser.storage.local.get("gs");
-        await new Promise((resolve) => setTimeout(resolve, 50));
+        await new Promise((resolve) => setTimeout(resolve, 25));
         if (getGs.gs.length === 0) {
             return;
         }
@@ -38,3 +27,14 @@ const updateBlockedUrlsListener = async () => {
         console.error("error updating blocked url listener. ERROR:", err);
     }
 };
+
+const handleStorageChange = (changes, areaName) => {
+    if (areaName === "local" && "gs" in changes) {
+        updateBlockedUrlsListener();
+    }
+};
+browser.storage.onChanged.addListener(handleStorageChange);
+
+browser.runtime.onInstalled.addListener(updateBlockedUrlsListener);
+browser.tabs.onActivated.addListener(updateBlockedUrlsListener);
+browser.tabs.onUpdated.addListener(updateBlockedUrlsListener);
